c986d9e6bbdae59a88573dfa6fec77cc5e7b4899
Abhibhava <manantyagi23@gmail.com>
2024-05-16 06:55:38 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@book-store-mern.nl4kgcb.mongodb.net/?retryWrites=true&w=majority&appName=BOOK-STORE-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
